de6d0f555dbe412e690af546e2264da91d2c4bb8
markoter <61563247+markoter@users.noreply.github.com>
2020-03-21 11:10:59 +0100

    add game over collider
    add collider-trigger under camera that triggers goto gameover scene (that with last index)

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class LoseCollider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void LoadGameOverScene()
     {
-        
+        int lastSceneIndex = SceneManager.sceneCountInBuildSettings - 1;
+        SceneManager.LoadScene(lastSceneIndex);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        LoadGameOverScene();
     }
 }
