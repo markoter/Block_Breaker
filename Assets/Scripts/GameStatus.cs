e01276adb0482420537977db3c691d97d1faa70a
markoter <61563247+markoter@users.noreply.github.com>
2020-04-02 17:57:38 +0200

    debug singleton patter
    gameObject.SetActive(false);

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -19,6 +19,7 @@ public class GameStatus : MonoBehaviour
         int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
         if (gameStatusCount > 1)
         {
+            gameObject.SetActive(false);
             Destroy(gameObject);
         }
         else
