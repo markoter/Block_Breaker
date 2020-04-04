937d3d3c0d272943322f062b3ecdf16abeb2b1a8
markoter <61563247+markoter@users.noreply.github.com>
2020-04-04 18:56:16 +0200

    rename gamestatus to gamesession

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -13,7 +13,7 @@ public class SceneLoader : MonoBehaviour
     public void LoadStartScene()
     {
         SceneManager.LoadScene(0);
-        FindObjectOfType<GameStatus>().ResetScore();
+        FindObjectOfType<GameSession>().ResetScore();
     }
     public void QuitGame()
     {
