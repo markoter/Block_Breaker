476f0df373017474cf4b7b8ea5a54b4e30312aed
markoter <61563247+markoter@users.noreply.github.com>
2020-04-05 15:40:40 +0200

    add prefabs to 2HP and 3HP blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -36,7 +36,10 @@ public class Block : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        HandleHit();
+        if (tag == "Breakable")
+        {
+            HandleHit();
+        }
     }
 
     private void HandleHit()
