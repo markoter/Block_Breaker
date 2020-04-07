674990ba1d3d54f3a03dae03f3115fdbb5d09998
markoter <61563247+markoter@users.noreply.github.com>
2020-04-07 18:30:10 +0200

    add simple autoplay

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -19,11 +19,23 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         //  Debug.Log(Input.mousePosition.x / Screen.width * screenWidthInUnits);
         Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
-        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
         // wyswietlanie pozycji paletki w konsoli Debug.Log(paddlePos.x);
         transform.position = paddlePos;
     }
+
+    private float GetXPos()
+    {
+        if (FindObjectOfType<GameSession>().IsAutoPlayEnabled())
+        {
+            return FindObjectOfType<Ball>().transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
+    }
+
 }
